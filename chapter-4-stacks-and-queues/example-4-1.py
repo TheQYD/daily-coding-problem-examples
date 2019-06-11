68b59715e839cd8eb16c6c59ef7eb709058360d1
Qasim <qasim@Qasims-MBP.home>
2019-06-10 21:56:38 -0400

    Completed example-4-1.py

---
+++ chapter-4-stacks-and-queues/example-4-1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, x):
+        self.stack.append(x)
+
+    def pop(self):
+        return self.stack.pop()
+
+    def max(self):
+        return max(self.stack)
+
+if __name__ == "__main__":
+
+    stack = Stack()
+    print "appending 1"
+    stack.push(1)
+    print "appending 2"
+    stack.push(2)
+    print "appending 3"
+    stack.push(3)
+
+    # Should print 3
+    print stack.max()
